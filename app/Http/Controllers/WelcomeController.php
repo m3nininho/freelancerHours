7e5378108715dc1818738682ea42648e0afbebfa
m3nininho <luiz.menino22@gmail.com>
2024-10-09 07:46:30 -0300

    Criação das tabelas e factorys. Banco já populado com dados fake para testes.

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+
+
+        return view('welcome');
+        //
+    }
+}
