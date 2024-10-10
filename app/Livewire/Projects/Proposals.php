44360f4b0e2fc4acf874956ff9aa801eb0518a27
m3nininho <luiz.menino22@gmail.com>
2024-10-10 17:57:46 -0300

    Feito a páginação da aplicação e o carregamento de mais propostas

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,11 +3,24 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Proposals extends Component
 {
     public Project $project;
+
+    public int $qty = 10;
+    
+    #[Computed()]
+    public function proposals(){
+        return $this->project->proposals()
+            ->orderByDesc('hours')
+            ->paginate($this->qty);
+    }
+    public function loadMore(){
+        $this->qty += 10;
+    }
     public function render()
     {
         return view('livewire.projects.proposals');
