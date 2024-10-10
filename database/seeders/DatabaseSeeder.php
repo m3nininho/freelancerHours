25c5f28c5f2fd470c77a1003b7ca0f9eabd19328
m3nininho <luiz.menino22@gmail.com>
2024-10-10 19:28:30 -0300

    Atualizando posições do ranking

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Actions\ArrangePositions;
 use App\Models\User;
 use App\Models\Project;
 use App\Models\Proposal;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -25,6 +27,8 @@ class DatabaseSeeder extends Seeder
             ]);
 
             Proposal::factory()->count(random_int(4,45))->create(['project_id' => $project->id]);
+
+            ArrangePositions::run($project->id);
         });
 
 
